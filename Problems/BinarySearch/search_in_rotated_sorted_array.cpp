2a1fcf614817064e52d004f7ce3b82ebdc92aa2e
Minh Nguyen <tmnguyen1403@gmail.com>
2022-09-18 16:52:21 -0400

    updated to be faster

--- Problems/BinarySearch/search_in_rotated_sorted_array.cpp
+++ Problems/BinarySearch/search_in_rotated_sorted_array.cpp
@@ -1,12 +1,14 @@
+#include <iostream>
+
 class Solution {
 public:
-    int16_t binarySearch(vector<int>& nums, const int& target, int16_t left, uint16_t right) {
-        while (left < right) {
+    int16_t binarySearch(vector<int>& nums, const int& target, int16_t left, int16_t right) {
+        while (left <= right) {
             auto mid = left + (right - left)/2;
             if (target > nums[mid])
                 left = mid + 1;
             else
-                right = mid;
+                right = mid - 1;
         }
         if (target == nums[left])
             return left;
@@ -22,20 +24,20 @@ public:
         int16_t left = 0;
         int16_t right = nums.size() - 1;
         int last_num = nums[right];
-        while (left < right) {
+        while (left <= right) {
             auto mid = left + (right - left)/ 2;
             if (nums[mid] > last_num)
                 left = mid + 1;
             else
-                right = mid;
+                right = mid - 1;
         }
         
         //perform binary search on [0, n-left] and [n-k+1, n)
         int16_t target_position = - 1;
-        if (target >= nums[0] && left > 0)
-            target_position = binarySearch(nums, target, 0, left - 1);
-        else if (target <= last_num)
+        if (target <= last_num)
             target_position = binarySearch(nums, target, left, nums.size()-1);
+        else if (target >= nums[0])
+            target_position = binarySearch(nums, target, 0, left - 1);
         
         return target_position;
     }
